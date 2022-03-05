309d67a6371716fd4e95137c720a120c55c1acd9
AlfonsoGonzalezOrtiz <0619973969@uma.es>
2022-03-05 21:27:06 +0100

    Fibonacci

--- src/main/java/Fibonacci.java
+++ src/main/java/Fibonacci.java
@@ -1,2 +1,28 @@
+
+/*
+ *  Class providing a method that computes the fibonacci of
+ *  an integer number
+ *  @author Alfonso Gonzalez Ortiz
+ */
+
 public class Fibonacci {
+
+    public int compute(int value) {
+        int inicio = 0;
+        int siguiente = 1;
+        int result = 1;
+        if(value < 0){
+            throw new RuntimeException("El numero introducido tiene un valor negativo: " + value);
+        }
+        else if(value == 0){
+            result = 0;
+        }
+        for(int i = 2; i <= value; i++){
+            result = inicio + siguiente;
+            inicio = siguiente;
+            siguiente = result;
+        }
+        return result;
+    }
 }
+
